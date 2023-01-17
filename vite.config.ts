d56ac392aecceb3ede0f1d6fa7524ded23068c23
Jake Donham <jaked@github.com>
2023-01-17 10:35:06 -0800

    run Vite with SSL

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,11 @@
 import { defineConfig } from "vite";
+import basicSsl from "@vitejs/plugin-basic-ssl";
 import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
+    basicSsl(),
     react(),
     {
       name: "webcontainer-headers",
@@ -11,7 +13,8 @@ export default defineConfig({
         server.middlewares.use((_req, res, next) => {
           res.setHeader("Cross-Origin-Embedder-Policy", "require-corp");
           res.setHeader("Cross-Origin-Opener-Policy", "same-origin");
-          // res.setHeader("Cross-Origin-Resource-Policy", "cross-origin");
+          // not in the docs but in the solid.js example
+          res.setHeader("Cross-Origin-Resource-Policy", "cross-origin");
           next();
         });
       },
